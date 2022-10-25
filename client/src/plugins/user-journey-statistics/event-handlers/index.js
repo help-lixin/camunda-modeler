09c4c1d5834529c293db47747f489f0644fc5da4
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-10-25 13:49:11 +0200

    feat(mixpanel): track form editor events
    Closes #3247

--- client/src/plugins/user-journey-statistics/event-handlers/index.js
+++ client/src/plugins/user-journey-statistics/event-handlers/index.js
@@ -11,9 +11,11 @@
 import DeploymentEventHandler from './DeploymentEventHandler';
 import LinkEventHandler from './LinkEventHandler';
 import TabEventHandler from './TabEventHandler';
+import FormEditorEventHandler from './FormEditorEventHandler';
 
 export default [
   DeploymentEventHandler,
+  FormEditorEventHandler,
   LinkEventHandler,
   TabEventHandler
 ];
